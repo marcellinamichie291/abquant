9a9342d4ca119e11ffe3d2629515d54a3ca94ec0
weijianan <weijianan@abakusglobal.com>
2021-08-03 17:29:55 +0800

    basic data structure

---
+++ abquant/trader/common.py
@@ -0,0 +1,95 @@
+
+from enum import Enum
+
+
+class Direction(Enum):
+    """
+    Direction of order/trade/position.
+    """
+    LONG = "多"
+    SHORT = "空"
+
+
+class Offset(Enum):
+    """
+    Offset of order/trade.
+    """
+    NONE = ""
+    OPEN = "开"
+    CLOSE = "平"
+
+
+class Status(Enum):
+    """
+    Order status.
+    """
+    SUBMITTING = "提交中"
+    NOTTRADED = "未成交"
+    PARTTRADED = "部分成交"
+    ALLTRADED = "全部成交"
+    CANCELLED = "已撤销"
+    REJECTED = "拒单"
+
+
+class Product(Enum):
+    """
+    Product class.
+    """
+    FUTURES = "期货"
+    OPTION = "期权"
+    SPOT = "现货"
+
+
+class OrderType(Enum):
+    """
+    Order type.
+    """
+    LIMIT = "限价"
+    MARKET = "市价"
+    STOP = "STOP"
+
+
+class OptionType(Enum):
+    """
+    Option type.
+    """
+    CALL = "看涨期权"
+    PUT = "看跌期权"
+
+
+class Exchange(Enum):
+    """
+    Exchange.
+    """
+    BITMEX = "BITMEX"
+    OKEX = "OKEX"
+    HUOBI = "HUOBI"
+    BITFINEX = "BITFINEX"
+    BINANCE = "BINANCE"
+    BYBIT = "BYBIT"         # bybit.com
+    COINBASE = "COINBASE"
+    DERIBIT = "DERIBIT"
+    GATEIO = "GATEIO"
+    BITSTAMP = "BITSTAMP"
+
+
+
+class Currency(Enum):
+    """
+    Currency.
+    """
+    USD = "USD"
+    HKD = "HKD"
+    CNY = "CNY"
+
+
+class Interval(Enum):
+    """
+    Interval of bar data.
+    """
+    MINUTE = "1m"
+    HOUR = "1h"
+    DAILY = "d"
+    WEEKLY = "w"
+    TICK = "tick"
+    CUSTOM = 'custom'
