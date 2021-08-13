e5ec187830cc0340313712a0e75cef28982245fb
weijianan <weijianan@abakusglobal.com>
2021-08-13 15:44:55 +0800

    refine congestion exception

--- abquant/trader/exception.py
+++ abquant/trader/exception.py
@@ -9,8 +9,9 @@ class MarketException(Exception):
 
 
 class CongestionException(Exception):
-    def __init__(self, threshold, message=r'Warning: the event congested in evend dispatcher is more than the threshold {} set. \
+    def __init__(self, threshold: int, congested_event: int, message: str = r'Warning: the number of event congested in evend dispatcher is now {} more than the threshold {} set. \
         Must you check any time-consuming operation or sync IO operation may used in your strategy implementation.'):
         self.threshold = threshold
-        self.message = message.format(self.threshold)
+        self.congested_event = congested_event
+        self.message = message.format(self.congested_event,self.threshold)
         super(CongestionException, self).__init__(self.message)
