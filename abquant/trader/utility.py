4ec987b98446169c72e3af74dc3c13665e490886
weijianan <weijianan@abakusglobal.com>
2021-08-13 16:15:25 +0800

    add some help function

--- abquant/trader/utility.py
+++ abquant/trader/utility.py
@@ -1,9 +1,14 @@
 import re
+from typing import Tuple
+
+from .common import Exchange
 
 ab_symbol_parrtern= re.compile(r'(.*)\.(.*)')
 
 def check_ab_symbol(ab_symbol: str) -> bool:
-    # chekc if a ab symbol valied
+    '''
+    chekc if a ab symbol valied
+    '''
     match = ab_symbol_parrtern.fullmatch(ab_symbol)
     if match:
         symbol_name, exchange_name = match.groups()
@@ -14,6 +19,21 @@ def check_ab_symbol(ab_symbol: str) -> bool:
     else:
         return False
 
+
+
+def extract_vt_symbol(ab_symbol: str) -> Tuple[str, Exchange]:
+    """
+    :return: (symbol, exchange)
+    """
+    symbol, exchange_str = ab_symbol.split(".")
+    return symbol, Exchange(exchange_str)
+
+
+def generate_vt_symbol(symbol: str, exchange: Exchange) -> str:
+    """
+    return vt_symbol
+    """
+    return f"{symbol}.{exchange.value}"
 
 class BarGenerator:
     def __init__(self, interval):
