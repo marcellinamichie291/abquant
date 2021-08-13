0b2fa7de33bd70987e2c59e3551e2eb437ab1d67
weijianan <weijianan@abakusglobal.com>
2021-08-13 15:04:48 +0800

    main part of event dispatcher

---
+++ abquant/event/dispatcher.py
@@ -0,0 +1,87 @@
+
+from collections import defaultdict
+from queue import Empty, Queue
+from threading import Thread
+import time
+from typing import Any, Callable, List
+
+from .event import EVENT_TIMER
+
+class Event:
+
+    def __init__(self, type: str, data: Any = None):
+        """"""
+        self.type: str = type
+        self.data: Any = data
+
+
+# Defines handler function to be used in event engine.
+HandlerType = Callable[[Event], None]
+
+
+class EventDispatcher:
+
+    def __init__(self, interval: int = 1):
+        self._interval: int = interval
+        self._queue: Queue = Queue()
+        self._active: bool = False
+        self._thread: Thread = Thread(target=self._run)
+        self._timer: Thread = Thread(target=self._run_timer)
+        self._handlers: defaultdict = defaultdict(list)
+        self._general_handlers: List = []
+
+    def _run(self) -> None:
+        while self._active:
+            try:
+                event = self._queue.get(block=True, timeout=1)
+                self._process(event)
+            except Empty:
+                pass
+
+    def _process(self, event: Event) -> None:
+        if event.type in self._handlers:
+            [handler(event) for handler in self._handlers[event.type]]
+
+        if self._general_handlers:
+            [handler(event) for handler in self._general_handlers]
+
+    def _run_timer(self) -> None:
+        while self._active:
+            time.sleep(self._interval)
+            event = Event(EVENT_TIMER)
+            self.put(event)
+
+    def start(self) -> None:
+        self._active = True
+        self._thread.start()
+        self._timer.start()
+
+    def stop(self) -> None:
+        self._active = False
+        self._timer.join()
+        self._thread.join()
+
+    def put(self, event: Event) -> None:
+        self._queue.put(event)
+
+    def register(self, type: str, handler: HandlerType) -> None:
+        handler_list = self._handlers[type]
+        if handler not in handler_list:
+            handler_list.append(handler)
+
+    def unregister(self, type: str, handler: HandlerType) -> None:
+        handler_list = self._handlers[type]
+
+        if handler in handler_list:
+            handler_list.remove(handler)
+
+        if not handler_list:
+            self._handlers.pop(type)
+
+    def register_general(self, handler: HandlerType) -> None:
+        if handler not in self._general_handlers:
+            self._general_handlers.append(handler)
+
+    def unregister_general(self, handler: HandlerType) -> None:
+        if handler in self._general_handlers:
+            self._general_handlers.remove(handler)
