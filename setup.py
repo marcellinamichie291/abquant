f214750fccc4efad17db7daa68423ac17f6f3dfa
weijianan <weijianan@abakusglobal.com>
2021-08-16 13:30:21 +0800

    add setup for one-line installation

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import PackageFinder, setup, find_packages
+
+setup(
+    name='abquant',
+    version='0.1',
+    description='event-driven quant dev tools',
+    author='independent regime',
+    # package_dir={"":"./abquant"},
+    # packages=find_packages(where='abquant'),
+    packages=find_packages(where='./'),
+    install_requires=[
+        'numpy',
+        'requests',
+        'websocket-client',
+        'pytz',
+
+    ],
+    python_requires='>=3.8',
+
+)
