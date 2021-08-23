5177024c2885175f6067319bb99bb552f87cc6a6
weijianan <weijianan@abakusglobal.com>
2021-08-23 14:36:12 +0800

    add explicit logger for sync debug

--- abquant/trader/utility.py
+++ abquant/trader/utility.py
@@ -1,5 +1,7 @@
 import re
-from typing import Tuple
+from typing import Tuple, Dict
+import logging
+from threading import Lock
 
 from .common import Exchange
 
@@ -39,6 +41,36 @@ def generate_vt_symbol(symbol: str, exchange: Exchange) -> str:
     """
     return f"{symbol}.{exchange.value}"
 
+
+
+file_handlers: Dict[str, logging.FileHandler] = {}
+
+log_formatter = logging.Formatter('[%(asctime)s] %(message)s')
+
+handler_mutex = Lock()
+
+def _get_file_logger_handler(filename: str) -> logging.FileHandler:
+    handler = file_handlers.get(filename, None)
+    if handler is None:
+        with handler_mutex:
+            handler = file_handlers.get(filename, None)
+            if handler is None:
+                handler = logging.FileHandler(filename)
+            file_handlers[filename] = handler  
+    return handler
+
+
+def get_file_logger(filename: str) -> logging.Logger:
+    logger = logging.getLogger(filename)
+    handler = _get_file_logger_handler(filename) 
+    handler.setFormatter(log_formatter)
+    logger.addHandler(handler) 
+    return logger
+
+
 if __name__ == '__main__':
     ab_test = 'syx'
+    print(ab_test)
     check_ab_symbol(ab_test)
+
+    get_file_logger('haha')
