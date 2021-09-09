9cd4f67836dcfcb83c028887ce04fef64d6aed5d
weijianan <weijianan@abakusglobal.com>
2021-09-09 12:33:12 +0800

    example query history

--- example/connectbinancec.py
+++ example/connectbinancec.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sys
 from pathlib import Path
 import time
@@ -5,8 +6,9 @@ import argparse
 
 from abquant.event import EventDispatcher, EventType, Event
 from abquant.gateway import BinanceBBCGateway, BinanceUBCGateway
-from abquant.trader.object import CancelRequest, OrderRequest, SubscribeMode, SubscribeRequest
-from abquant.trader.common import Direction, Exchange, Offset, OrderType
+from abquant.trader.object import CancelRequest, HistoryRequest, OrderRequest, SubscribeMode, SubscribeRequest
+from abquant.trader.common import Direction, Exchange, Interval, Offset, OrderType
+
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -48,21 +50,21 @@ if __name__ == '__main__':
         str('ORDER: ') + str(event.data)))
     event_dispatcher.register(EventType.EVENT_TRADE, lambda event: print(
         str('TRADE: ') + str(event.data)))
-    event_dispatcher.register(EventType.EVENT_TICK, lambda event: print(
-        str('TICK: ') + str(event.data)))
-    event_dispatcher.register(EventType.EVENT_DEPTH, lambda event: print(
-        str('DEPTH: ') + str(event.data)))
-    event_dispatcher.register(EventType.EVENT_TRANSACTION, lambda event: print(
-        str('TRANSACTION: ') + str(event.data)))
+    # event_dispatcher.register(EventType.EVENT_TICK, lambda event: print(
+    #     str('TICK: ') + str(event.data)))
+    # event_dispatcher.register(EventType.EVENT_DEPTH, lambda event: print(
+    #     str('DEPTH: ') + str(event.data)))
+    # event_dispatcher.register(EventType.EVENT_TRANSACTION, lambda event: print(
+    #     str('TRANSACTION: ') + str(event.data)))
     # event_dispatcher.register(EventType.EVENT_ENTRUST, lambda event:  print(str('ENTRUST: ') + str(event.data)))
     # event_dispatcher.register_general(lambda event: print(str(event.type) +  str(event.data)))
+
+    # 订阅行情
     gateway = BinanceUBCGateway(event_dispatcher)
+    gateway_ = BinanceBBCGateway(event_dispatcher)
     gateway.connect(binance_setting)
-
+    gateway_.connect(binance_setting)
     time.sleep(3)
-
-    from abquant.gateway.binancec import symbol_contract_map
-
     gateway.set_subscribe_mode(SubscribeMode(
         depth=False,
         tick_5=False,
@@ -72,18 +74,38 @@ if __name__ == '__main__':
     )
     gateway.subscribe(SubscribeRequest(
         symbol='ICPUSDT', exchange=Exchange.BINANCE))
+
+    from abquant.gateway.binancec import symbol_contract_map
     for i, k in enumerate(symbol_contract_map):
         # if i > 0:
         #     break
         print(i, k)
-        gateway.subscribe(SubscribeRequest(
-            symbol=k, exchange=Exchange.BINANCE))
-
-    # order_id: str = gateway.send_order(OrderRequest(symbol='XRPUSDT', exchange=Exchange.BINANCE, direction=Direction.SHORT, type=OrderType.LIMIT, volume=2, price=1.3, offset=Offset.OPEN))
-    gateway.start()
+    #     gateway.subscribe(SubscribeRequest(
+    #         symbol=k, exchange=Exchange.BINANCE))
+    # gateway.start()
     print("start to receive data from exchange")
-    time.sleep(10)
 
-    # gateway.cancel_order(CancelRequest(order_id.split('.')[-1], symbol='XRPUSDT', exchange=Exchange.BINANCE))
+    # 下单撤单
+    # ab_order_id: str = gateway.send_order(OrderRequest(symbol='XRPUSDT', exchange=Exchange.BINANCE,
+    #                                       direction=Direction.SHORT, type=OrderType.LIMIT, volume=2, price=1.3, offset=Offset.OPEN))
+    # print('ab orderid', ab_order_id)
+    # time.sleep(10)
+    # order_id = ab_order_id.split('.')[-1]
+    # gateway.cancel_order(CancelRequest(
+    #     order_id, symbol='XRPUSDT', exchange=Exchange.BINANCE))
+    # print('orderid', order_id)
+
+
+
+
+
+    # 查询历史
+    history = gateway_.query_history(HistoryRequest(
+        symbol='BTCUSD_PERP', exchange=Exchange.BINANCE,
+        start=datetime(year=2021, month=9, day=5, hour=0, minute=0),
+        end=datetime(year=2021, month=9, day=6, hour=0, minute=0),
+        interval=Interval.MINUTE))
+    print(history)
+
     while True:
         time.sleep(1)
