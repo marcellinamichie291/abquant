b5e0f04d915e6b28b6016a65e68041b364ec9831
weijianan <weijianan@abakusglobal.com>
2021-09-09 15:46:02 +0800

    refine example

--- example/connectbinancec.py
+++ example/connectbinancec.py
@@ -31,11 +31,15 @@ if __name__ == '__main__':
         "key": args.key,
         "secret": args.secret,
         "session_number": 3,
+        # "127.0.0.1" str类型
         "proxy_host": args.proxy_host if args.proxy_host else "",
+        # 1087 int类型
         "proxy_port": args.proxy_port if args.proxy_port else 0,
         "test_net": ["TESTNET", "REAL"][1],
     }
     event_dispatcher = EventDispatcher()
+
+    # 根据事件类型 注册回调函数。 随意uncomment
     event_dispatcher.register(EventType.EVENT_LOG, lambda event: print(
         str('LOG: ') + str(event.data)))  # pass
     # event_dispatcher.register(EventType.EVENT_TIMER, lambda event:  print(str('TIMER: ') + str(event.data))) #pass
@@ -54,58 +58,69 @@ if __name__ == '__main__':
     #     str('TICK: ') + str(event.data)))
     # event_dispatcher.register(EventType.EVENT_DEPTH, lambda event: print(
     #     str('DEPTH: ') + str(event.data)))
-    # event_dispatcher.register(EventType.EVENT_TRANSACTION, lambda event: print(
-    #     str('TRANSACTION: ') + str(event.data)))
+    event_dispatcher.register(EventType.EVENT_TRANSACTION, lambda event: print(
+        str('TRANSACTION: ') + str(event.data)))
     # event_dispatcher.register(EventType.EVENT_ENTRUST, lambda event:  print(str('ENTRUST: ') + str(event.data)))
     # event_dispatcher.register_general(lambda event: print(str(event.type) +  str(event.data)))
 
     # 订阅行情
+    # u本位 gateway
     gateway = BinanceUBCGateway(event_dispatcher)
+    # btc 本位 gateway
     gateway_ = BinanceBBCGateway(event_dispatcher)
     gateway.connect(binance_setting)
     gateway_.connect(binance_setting)
     time.sleep(3)
     gateway.set_subscribe_mode(SubscribeMode(
+        #订阅 深度数据 depth
         depth=False,
+        # 最优五档tick
         tick_5=False,
+        # best bid/ask tick
         best_tick=False,
+        # 委托单（通常不支持） entrust
         entrust=False,
+        # 交易数据 transaction
         transaction=True)
     )
     gateway.subscribe(SubscribeRequest(
         symbol='ICPUSDT', exchange=Exchange.BINANCE))
 
+    # gateway.connect 之后会更新的 binance合约交易的 合约的dict,  symbol_contract_map是全局的一个单例。
     from abquant.gateway.binancec import symbol_contract_map
+
     for i, k in enumerate(symbol_contract_map):
-        # if i > 0:
-        #     break
-        print(i, k)
-    #     gateway.subscribe(SubscribeRequest(
-    #         symbol=k, exchange=Exchange.BINANCE))
-    # gateway.start()
+        if i > 3:
+            break
+        print(i, k, symbol_contract_map[k])
+        gateway.subscribe(SubscribeRequest(
+            symbol=k, exchange=Exchange.BINANCE))
+    # subscribe 各个产品后 要调用gateway.start 开始接受数据。该操作较为冗赘，有实现细节上的考虑。 实现strategy时，以上调用对交易员隐藏，有框架实现。
+    gateway.start()
     print("start to receive data from exchange")
 
     # 下单撤单
-    # ab_order_id: str = gateway.send_order(OrderRequest(symbol='XRPUSDT', exchange=Exchange.BINANCE,
-    #                                       direction=Direction.SHORT, type=OrderType.LIMIT, volume=2, price=1.3, offset=Offset.OPEN))
-    # print('ab orderid', ab_order_id)
-    # time.sleep(10)
-    # order_id = ab_order_id.split('.')[-1]
-    # gateway.cancel_order(CancelRequest(
-    #     order_id, symbol='XRPUSDT', exchange=Exchange.BINANCE))
-    # print('orderid', order_id)
+    ab_order_id: str = gateway.send_order(OrderRequest(symbol='XRPUSDT', exchange=Exchange.BINANCE,
+                                          direction=Direction.LONG, type=OrderType.LIMIT, volume=7, price=1.09, offset=Offset.OPEN))
+    print('ab orderid', ab_order_id)
+    time.sleep(10)
+    order_id = ab_order_id.split('.')[-1]
+    print('orderid', order_id)
+    gateway.cancel_order(CancelRequest(
+        order_id, symbol='XRPUSDT', exchange=Exchange.BINANCE))
 
 
 
 
 
-    # 查询历史
+    # # 查询历史
     history = gateway_.query_history(HistoryRequest(
         symbol='BTCUSD_PERP', exchange=Exchange.BINANCE,
         start=datetime(year=2021, month=9, day=5, hour=0, minute=0),
         end=datetime(year=2021, month=9, day=6, hour=0, minute=0),
         interval=Interval.MINUTE))
-    print(history)
+    
+    print(history[1:3])
 
     while True:
         time.sleep(1)
