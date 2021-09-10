2871ddf2a5bee5af68cc56022ae291726e152017
weijianan <weijianan@abakusglobal.com>
2021-09-10 16:30:14 +0800

    basic api

--- abquant/strategytrading/livestrategyrunner.py
+++ abquant/strategytrading/livestrategyrunner.py
@@ -1,3 +1,89 @@
+from typing import Iterable
+from logging import INFO
+from abquant.trader.common import Direction, Interval, Offset
+from abquant.event import EventDispatcher
 
-class StrategyEngine:
-    pass
+from .template import StrategyTemplate
+
+LOG_LEVEL = int
+
+class LiveStrategyRunner:
+
+    def __init__(self, event_dispatcher: EventDispatcher):
+        pass
+
+    def init(self):
+        pass
+
+    def close(self):
+        pass
+
+    def register_event(self):
+        # register event to process_xx method
+        pass
+
+    def process_xx(self, xx: xxDATA):
+        pass
+
+    def send_order(self,
+                   strategy: StrategyTemplate,
+                   ab_symbol: str,
+                   direction: Direction,
+                   offset: Offset,
+                   price: float,
+                   volume: float):
+        pass
+
+    def cancel_order(self, strategy: StrategyTemplate, ab_orderid: str):
+        pass
+
+    def cancel_orders(self, strategy: StrategyTemplate, ab_orderids: Iterable[str]):
+        pass
+
+    # I guess it is not necessary.
+    # def cancel_all(self) ->None:
+    #     pass
+
+    def load_bars(self,
+                  strategy: StrategyTemplate,
+                  days: int,
+                  interval: Interval = Interval.MINUTE):
+        pass
+
+    def add_strategy(
+            self,
+            strategy_class: type,
+            strategy_name: str,
+            ab_symbols: list,
+            setting: dict):
+        pass
+    
+    def edit_strategy(self, strategy_name: str, setting: dict):
+        pass
+
+    def get_strategy(self, strategy_name: str):
+        pass
+
+    def remove_strategy(self, strategy_name: str):
+        pass
+
+    def init_strategy(self, strategy_name: str):
+        pass
+
+    def start_strategy(self, strategy_name: str):
+        pass
+
+    def stop_strategy(self, strategy_name: str):
+        pass
+
+    def init_all_strategies(self):
+        pass
+
+    def stop_all_strategies(self):
+        pass
+
+    def write_log(self, msg: str, strategy: StrategyTemplate = None, level:LOG_LEVEL=INFO):
+        pass
+    
+    def phone_call(self, msg: str, strategy: StrategyTemplate):
+        pass
--- abquant/trader/tool.py
+++ abquant/trader/tool.py
@@ -1,9 +1,73 @@
 
 
+from abquant.trader.common import Interval
+from abquant.trader.msg import BarData, TickData, TransactionData
+from typing import Callable, Optional
+
+
 class BarGenerator:
     def __init__(self, interval):
         pass
 
+class BarGenerator:
+    """
+    For:
+    1. generating 1 minute bar data from tick data
+    2. generateing x minute bar/x hour bar data from 1 minute data
+
+    Notice:
+    1. for x minute bar, x must be able to divide 60: 2, 3, 5, 6, 10, 15, 20, 30
+    2. for x hour bar, x can be any number
+    """
+
+    def __init__(
+        self,
+        on_bar: Callable,
+        window: int = 0,
+        on_window_bar: Callable = None,
+        interval: Interval = Interval.MINUTE
+    ):
+        self.bar: BarData = None
+        self.on_bar: Callable = on_bar
+
+        self.interval: Interval = interval
+        self.interval_count: int = 0
+
+        self.hour_bar: BarData = None
+
+        self.window: int = window
+        self.window_bar: BarData = None
+        self.on_window_bar: Callable = on_window_bar
+
+        self.last_tick: TickData = None
+        self.last_bar: BarData = None
+
+        self.last_transaction: TransactionData = None
+
+
+    def update_tick(self, tick: TickData) -> None:
+        """
+        either update_tick or update_transaction. use both at same time is not allowed. 
+        """
+        pass
+
+    def update_transaction(self, tick: TransactionData) -> None:
+        pass
+
+    def update_bar(self, bar: BarData) -> None:
+        pass
+
+    def update_bar_minute_window(self, bar: BarData) -> None:
+        pass
+
+    def update_bar_hour_window(self, bar: BarData) -> None:
+        pass
+
+    def on_hour_bar(self, bar: BarData) -> None:
+        pass
+
+    def generate(self) -> Optional[BarData]:
+        pass
 
 class ArrayCache:
     def __init__(self, size: int):
