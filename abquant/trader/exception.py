05b75cead5b7289d919d49d27dd460539cb27789
weijianan <weijianan@abakusglobal.com>
2021-09-14 14:57:50 +0800

    exception refine and utilty renew

--- abquant/trader/exception.py
+++ abquant/trader/exception.py
@@ -1,18 +1,17 @@
+class GatewayException(Exception):
+    def __init__(self, message: str, gateway_name: str) -> None:
+        super().__init__(message, gateway_name=gateway_name)
 
-
-class OrderException(Exception):
+class OrderException(GatewayException):
     pass
 
-
-class MarketException(Exception):
-    def __init__(self, message: str):
-        super(MarketException, self).__init__(message)
-
+class MarketException(GatewayException):
+    pass
 
 class CongestionException(Exception):
     def __init__(self, threshold: int, congested_event: int, message: str = r'Warning: the number of event congested in evend dispatcher is now {} more than the threshold {} set. \
         Must you check any time-consuming operation or sync IO operation may used in your strategy implementation.'):
         self.threshold = threshold
         self.congested_event = congested_event
-        self.message = message.format(self.congested_event,self.threshold)
+        self.message = message.format(self.congested_event, self.threshold)
         super(CongestionException, self).__init__(self.message)
--- abquant/trader/utility.py
+++ abquant/trader/utility.py
@@ -1,13 +1,17 @@
+from collections import defaultdict
 import re
-from typing import Tuple, Dict
+from typing import Iterable, List, Tuple, Dict
 import logging
 from threading import Lock
 from decimal import Decimal
 import math
 
+from abquant.trader.msg import OrderData
+
 from .common import Exchange
 
-ab_symbol_parrtern= re.compile(r'(.*)\.(.*)')
+ab_symbol_parrtern = re.compile(r'(.*)\.(.*)')
+
 
 def check_ab_symbol(ab_symbol: str) -> bool:
     '''
@@ -17,16 +21,15 @@ def check_ab_symbol(ab_symbol: str) -> bool:
     if match:
         symbol_name, exchange_name = match.groups()
         try:
-        # checkout symbol_name
+            # checkout symbol_name
             Exchange(exchange_name)
         except ValueError as e:
             return False
 
         return True
     else:
-  
-        return False
 
+        return False
 
 
 def extract_ab_symbol(ab_symbol: str) -> Tuple[str, Exchange]:
@@ -44,12 +47,12 @@ def generate_ab_symbol(symbol: str, exchange: Exchange) -> str:
     return f"{symbol}.{exchange.value}"
 
 
-
 file_handlers: Dict[str, logging.FileHandler] = {}
 
 log_formatter = logging.Formatter('[%(asctime)s] %(message)s')
 
 handler_mutex = Lock()
+
 
 def _get_file_logger_handler(filename: str) -> logging.FileHandler:
     handler = file_handlers.get(filename, None)
@@ -58,15 +61,15 @@ def _get_file_logger_handler(filename: str) -> logging.FileHandler:
             handler = file_handlers.get(filename, None)
             if handler is None:
                 handler = logging.FileHandler(filename)
-            file_handlers[filename] = handler  
+            file_handlers[filename] = handler
     return handler
 
 
 def get_file_logger(filename: str) -> logging.Logger:
     logger = logging.getLogger(filename)
-    handler = _get_file_logger_handler(filename) 
+    handler = _get_file_logger_handler(filename)
     handler.setFormatter(log_formatter)
-    logger.addHandler(handler) 
+    logger.addHandler(handler)
     return logger
 
 
@@ -77,12 +80,12 @@ def round_to(value: float, target: float) -> float:
     return rounded
 
 
-
 def round_up(value: float, target: float) -> float:
     value = Decimal(str(value))
     target = Decimal(str(target))
     rounded = float(int(math.ceil(value / target)) * target)
     return rounded
+
 
 def round_down(value: float, target: float) -> float:
     value = Decimal(str(value))
@@ -90,9 +93,34 @@ def round_down(value: float, target: float) -> float:
     rounded = float(int(math.floor(value / target)) * target)
     return rounded
 
+
+class OrderGrouper:
+    def __init__(self):
+        self.gateway_name_orders_map: Dict[str: List[OrderData]] = defaultdict(list)
+
+    def add(self, order: OrderData) -> None:
+        self.gateway_name_orders_map[order.gateway_name].append(order)
+    
+    def get(self, gateway_name: str) -> Dict[str, List[OrderData]] :
+        return self.gateway_name_orders_map[gateway_name]
+    
+    def __getitem__(self, gateway_name: str) -> List[OrderData]:
+        return self.gateway_name_orders_map[gateway_name]
+    
+    def items(self) -> Iterable[Tuple[str, List[OrderData]]]:
+        return self.gateway_name_orders_map.items()
+
+
 if __name__ == '__main__':
     ab_test = 'syx'
     print(ab_test)
     check_ab_symbol(ab_test)
 
     get_file_logger('haha')
+
+    grouper = OrderGrouper()
+    grouper.add(OrderData(gateway_name='A', symbol='a', exchange=Exchange.BINANCE, orderid='1'))
+    grouper.add(OrderData(gateway_name='A', symbol='b', exchange=Exchange.BINANCE, orderid='2'))
+    grouper.add(OrderData(gateway_name='B', symbol='a', exchange=Exchange.BINANCE, orderid='3'))
+    for name, order in grouper.items():
+        print(name, order)
