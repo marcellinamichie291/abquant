5d0a4fbdec0abeb9d0c3cccc5f34acdd481210a5
狄最 <dizui@abakusglobal.com>
2021-10-26 10:03:06 +0800

    [mod] on_depth修改，事件测试

--- connect_dydx.py
+++ connect_dydx.py
@@ -14,7 +14,7 @@ dydx_setting = {
     "secret": "NLrG3Kuyspe0jt45gENM6UmWGLdSrwj88P-5UPrz",
     "passphrase": "rOkw33sCBiTQhE5PX_GR",
     "stark_private_key": "01a65d7c5fccd96786b0a42ba38df41f58383fcdace4be8581487b61739cc559",
-    # "walletAddress":"0xf02f589bBaB91a8609D896aBeC1f8e4E5D1165c3",
+    "walletAddress":"0xf02f589bBaB91a8609D896aBeC1f8e4E5D1165c3",
     "proxy_host": "",
     "proxy_port": 0,
     "test_net": ["TESTNET", "REAL"][1],
@@ -22,30 +22,61 @@ dydx_setting = {
     "accountNumber": 0
 }
 
+# dydx_setting = {
+#     "key": "0ec5b370-74d1-be64-a578-b7bb085ac937",
+#     "secret": "NLrG3Kuyspe0jt45gENM6UmWGLdSrwj88P-5UPrz",
+#     "passphrase": "rOkw33sCBiTQhE5PX_GR",
+#     "stark_private_key": "01a65d7c5fccd96786b0a42ba38df41f58383fcdace4be8581487b61739cc559",
+#     # "walletAddress":"0xf02f589bBaB91a8609D896aBeC1f8e4E5D1165c3",
+#     "proxy_host": "",
+#     "proxy_port": 0,
+#     "test_net": ["TESTNET", "REAL"][1],
+#     "limitFee": 0.001, # makerFeeRate: 0.00050 or takerFeeRate: takerFeeRate: 0.00100
+#     "accountNumber": 0
+# }
+
 event_dispatcher = EventDispatcher()
 
 # 根据事件类型 注册回调函数。 随意uncomment
 event_dispatcher.register(EventType.EVENT_LOG, lambda event: print(
     str('LOG: ') + str(event.data)))  # pass
+
 # event_dispatcher.register(EventType.EVENT_TIMER, lambda event:  print(str('TIMER: ') + str(event.data))) #pass
-event_dispatcher.register(EventType.EVENT_ACCOUNT, lambda event: print(
-    str('ACCOUNT: ') + str(event.data)))  # pass accessor,  trade_listerer not done
+
+# ok
+# event_dispatcher.register(EventType.EVENT_ACCOUNT, lambda event: print(
+#     str('ACCOUNT: ') + str(event.data)))  # pass accessor,  trade_listerer not done
+
+# ok
 # event_dispatcher.register(EventType.EVENT_CONTRACT, lambda event:  print(str('CONTRACT: ') + str(event.data))) # pass
-event_dispatcher.register(EventType.EVENT_POSITION, lambda event: print(
-    str('POSITION: ') + str(event.data)))  # pass accessor, trade_listerer not done
-event_dispatcher.register(EventType.EVENT_EXCEPTION, lambda event: print(
-    str('EXCEPTION: ') + str(event.data)))
-event_dispatcher.register(EventType.EVENT_ORDER, lambda event: print(
-    str('ORDER: ') + str(event.data)))
-event_dispatcher.register(EventType.EVENT_TRADE, lambda event: print(
-    str('TRADE: ') + str(event.data)))
+
+# ok need test
+# event_dispatcher.register(EventType.EVENT_POSITION, lambda event: print(
+#     str('POSITION: ') + str(event.data)))  # pass accessor, trade_listerer not done
+
+
+# event_dispatcher.register(EventType.EVENT_EXCEPTION, lambda event: print(
+#     str('EXCEPTION: ') + str(event.data)))
+
+# ok
+# event_dispatcher.register(EventType.EVENT_ORDER, lambda event: print(
+#     str('ORDER: ') + str(event.data)))
+
+# need test
+# event_dispatcher.register(EventType.EVENT_TRADE, lambda event: print(
+#     str('TRADE: ') + str(event.data)))
+
+# ok
 # event_dispatcher.register(EventType.EVENT_TICK, lambda event: print(
 #     str('TICK: ') + str(event.data)))
+
+# ok
 event_dispatcher.register(EventType.EVENT_DEPTH, lambda event: print(
     str('DEPTH: ') + str(event.data)))
-event_dispatcher.register(EventType.EVENT_TRANSACTION, lambda event: print(
-    str('TRANSACTION: ') + str(event.data)))
-event_dispatcher.register(EventType.EVENT_ENTRUST, lambda event:  print(str('ENTRUST: ') + str(event.data)))
+
+# event_dispatcher.register(EventType.EVENT_TRANSACTION, lambda event: print(
+#     str('TRANSACTION: ') + str(event.data)))
+# event_dispatcher.register(EventType.EVENT_ENTRUST, lambda event:  print(str('ENTRUST: ') + str(event.data)))
 # event_dispatcher.register_general(lambda event: print(str(event.type) +  str(event.data)))
 
 dydx_gateway = DydxGateway(event_dispatcher)
@@ -61,19 +92,19 @@ dydx_gateway.subscribe(SubscribeRequest(
     symbol="BTC-USD", exchange=Exchange.DYDX
 ))
 
-from abquant.gateway.dydx import symbol_contract_map
+# from abquant.gateway.dydx import symbol_contract_map
 
 # print(symbol_contract_map)
 
 dydx_gateway.start()
 
 # 下单撤单
-# for i in range(5):
-#     order_id = dydx_gateway.send_order(OrderRequest(symbol="BTC-USD", exchange=Exchange.DYDX,
-#                                 direction=Direction.LONG, type=OrderType.LIMIT, 
-#                                 price=60000, volume=0.01, offset=Offset.OPEN))
-#     print("@@@open_long",order_id)
-#     dydx_gateway.cancel_order(CancelRequest(order_id, symbol="BTC-USD",exchange=Exchange.DYDX))
+for i in range(5):
+    order_id = dydx_gateway.send_order(OrderRequest(symbol="BTC-USD", exchange=Exchange.DYDX,
+                                direction=Direction.LONG, type=OrderType.LIMIT, 
+                                price=60000, volume=0.01, offset=Offset.OPEN))
+    print("@@@open_long",order_id)
+    dydx_gateway.cancel_order(CancelRequest(order_id, symbol="BTC-USD",exchange=Exchange.DYDX))
 
 
 
