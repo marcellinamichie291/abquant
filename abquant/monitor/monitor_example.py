b89b16a2a799b5dac58718b8dd97ce96b21fc46e
zhanghui <zhanghui@abakusglobal.com>
2021-11-01 15:09:43 +0800

    monitor example

---
+++ abquant/monitor/monitor_example.py
@@ -0,0 +1,39 @@
+import argparse
+import time
+
+from abquant.monitor import Monitor
+from abquant.trader.msg import BarData, DepthData, EntrustData, OrderData, TickData, TradeData, TransactionData
+
+
+
+
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--username', type=str, required=True,
+                        help='username')
+    parser.add_argument('-w', '--password', type=str, required=True,
+                        help='password')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse()
+    common_setting = {
+        "username": args.username,
+        "password": args.password,
+    }
+    # Monitor.init_monitor(common_setting)
+    monitor = Monitor(common_setting)
+    monitor.start()
+
+    i = 0
+    while True:
+        time.sleep(10)
+        i += 1
+        monitor.send(f'message {i}')
+
+
+if __name__ == '__main__':
+    main()
