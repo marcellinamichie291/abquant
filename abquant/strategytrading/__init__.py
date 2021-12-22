8038b4bc639d6570f6ba08a439d9872f99bdce61
weijianan <weijianan@abakusglobal.com>
2021-12-22 16:45:47 +0800

    backtest api renew. and change the market order to limit in doubleMA

--- abquant/strategytrading/__init__.py
+++ abquant/strategytrading/__init__.py
@@ -1,12 +1,70 @@
+from collections import defaultdict
+from datetime import datetime
 from enum import Enum
+from typing import DefaultDict, Dict, List
+
+from abquant.trader.common import Interval
 
 from .template import StrategyTemplate
 from .livestrategyrunner import LiveStrategyRunner
-from .backteststrategyrunner import BacktestStrategyRunner
-
 
 
 class BacktestingMode(Enum):
     BAR = "bar"
     TICK = "tick"
     Non = "live"
+
+
+class BacktestParameter:
+    def __init__(
+        self,
+        ab_symbols: List[str],
+        interval: Interval,
+        rates: Dict[str, float],
+        slippages: Dict[str, float],
+        sizes: Dict[str, float],
+        priceticks: Dict[str, float],
+        capital: int = 0,
+        risk_free: float = 0,
+        inverses: DefaultDict[str, bool] = defaultdict(lambda: False),
+        mode: BacktestingMode = BacktestingMode.BAR,
+        annual_days: int = 365,
+    ):
+        self.ab_symbols: List[str] = ab_symbols
+        self.interval: Interval = interval
+
+        self.rates: Dict[str, float] = rates
+        self.slippages: Dict[str, float] = slippages
+        self.sizes: Dict[str, float] = sizes
+        self.priceticks: Dict[str, float] = priceticks
+
+        self.capital: int = capital
+        self.risk_free: float = risk_free
+        self.inverse: DefaultDict[str, bool] = inverses
+
+        self.mode: BacktestingMode = mode
+        self.annual_days: int = annual_days
+
+    # TODO checkout inverse 
+    # def 
+
+    def sub_parameter(self, ab_symbols: List[str]) -> "BacktestParameter":
+
+        def sub_dict(keys, dict):
+            sub_dict = {key: dict[key] for key in keys}
+            return sub_dict
+
+        return BacktestParameter(
+            ab_symbols=ab_symbols,
+            interval=self.interval,
+            rates=sub_dict(ab_symbols, self.rates),
+            slippages=sub_dict(ab_symbols, self.slippages),
+            sizes=sub_dict(ab_symbols, self.sizes),
+            priceticks=sub_dict(ab_symbols, self.priceticks),
+            capital=self.capital,
+            inverses=self.inverse,
+            annual_days=self.annual_days,
+            mode=self.mode
+        )
+
+from .backteststrategyrunner import BacktestStrategyRunner
--- example/run_cta_backtest.py
+++ example/run_cta_backtest.py
@@ -1,27 +1,32 @@
 
 from datetime import datetime
+import time
 from abquant.dataloader import DataLoaderKline
-from abquant.strategytrading import BacktestStrategyRunner, BacktestingMode
+from abquant.dataloader.dataloader import DataLoader
+from abquant.strategytrading import BacktestParameter, BacktestStrategyRunner, BacktestingMode
 from abquant.trader.common import Interval
 from strategy.doublemeanaverage import DoubleMAStrategy
 
 
+from abquant.dataloader.dfdataloader import DFDataLoader
 if __name__ == '__main__':
-    dl_setting = {}
-    start = datetime(2021, 12, 1)
-    end = datetime(2020, 12, 12)
-    dataloader: DataLoaderKline = DataLoaderKline(dl_setting)
-    # dataset = dataloader.load_data('ETHUSDT.BINANCE', start, end)
+    dl_setting = {
+        "dir_path": '/Users/abakus/Desktop/projects/abquant/example_my/binance_history_data_all'
+    }
 
-    backtest_strategy_runner = BacktestStrategyRunner()
-    backtest_strategy_runner.set_data_loader(dataloader)
+    start = datetime(2021, 8, 1)
+    end = datetime(2021, 9, 12)
+    dataloader: DataLoaderKline = DataLoaderKline(dl_setting)
+    # dataset = dataloader.load_data('BTCUSDT.BINANCE', start, end)
+    # for bar in dataset:
+    #     print(bar)
+    #     time.sleep(1)
 
     ab_symbol = 'BTCUSDT.BINANCE'
 
-    backtest_strategy_runner.set_parameters(
+    backtest_parameter = BacktestParameter(
         ab_symbols=[ab_symbol],
         interval=Interval.MINUTE,
-        start=start,
         rates={
             ab_symbol: 0.0003
         },
@@ -33,19 +38,22 @@ if __name__ == '__main__':
         },
         priceticks={
             ab_symbol: 0.001
-            },
-        capital=10000,
-        inverse={
+        },
+        capital=200000,
+        inverses={
             ab_symbol: False
-            },
-        end=end,
+        },
         annual_days=365,
         mode=BacktestingMode.BAR
     )
+    backtest_strategy_runner = BacktestStrategyRunner()
+    backtest_strategy_runner.set_data_loader(dataloader)
+    backtest_strategy_runner.set_parameter(backtest_parameter)
+
     backtest_strategy_runner.add_strategy(strategy_class=DoubleMAStrategy,
                                           strategy_name='strategy_1',
-                                          ab_symbols=["XBTUSD.BITMEX"],
+                                          ab_symbols=[ab_symbol],
                                           setting={}
                                           )
 
-    backtest_strategy_runner.run_backtest()
+    backtest_strategy_runner.run_backtest(start_dt=start, end_dt=end, output_log=True)
