9590eb67dea936ec2d8a0fd46d7177a76efd7968
weijianan <weijianan@abakusglobal.com>
2022-01-13 14:18:16 +0800

    0.10.0

--- abquant/strategytrading/backteststrategyrunner.py
+++ abquant/strategytrading/backteststrategyrunner.py
@@ -1,11 +1,9 @@
 from collections import defaultdict
-from typing import DefaultDict, Dict, Iterable, List, Tuple
+from typing import Dict, Iterable, List, OrderedDict, Text, Tuple, Type
 from datetime import datetime
+from collections import OrderedDict as OrderedDictionary
 
-from pandas.tseries.offsets import Second
 from abquant.ordermanager import OrderManager
-from abquant.strategytrading import replayrunner
-from abquant.strategytrading.replayrunner import ReplayRunner
 from abquant.trader.common import Direction, Interval, Offset, OrderType
 from abquant.trader.exception import MarketException
 from abquant.event import EventType, Event, EventDispatcher
@@ -16,52 +14,55 @@ from abquant.dataloader import DataLoader
 from . import BacktestParameter, BacktestingMode
 from .template import StrategyTemplate
 from .strategyrunner import StrategyManager, StrategyRunner, LOG_LEVEL
-
-
+from .replayrunner import ReplayRunner
+from .result import ContractsDailyResult
 
 
 class BacktestStrategyRunner(StrategyManager):
     def __init__(self) -> None:
         self.strategies: Dict[str, StrategyTemplate] = {}
-        self.replay_runner: Dict[str, ReplayRunner] = {}
+        self.replay_runners: Dict[str, ReplayRunner] = {}
         self.data_loader: DataLoader = None
         self.parameter: BacktestParameter = None
 
     def set_data_loader(self, data_loader: DataLoader):
         self.data_loader = data_loader
-    
+
     def set_parameter(self, backtest_parameter: BacktestParameter):
         self.parameter = backtest_parameter
 
-    def add_strategy(self, strategy_class: type, strategy_name: str, ab_symbols: list, setting: dict) -> None:
+    def add_strategy(self, strategy_class: Type, strategy_name: str, ab_symbols: list, setting: dict) -> None:
         if self.strategies:
             raise NotImplementedError(
                 "only 1 strategy instance backtesting is supported")
+        if strategy_name in self.strategies:
+            raise ValueError("strategy_name: {} already exists".format(strategy_name))
 
         try:
             sub_parameter = self.parameter.sub_parameter(ab_symbols)
         except KeyError as e:
             raise KeyError(f"{e} is not in backtest parameter")
         replay_runner = ReplayRunner(
-                ab_symbols=ab_symbols,
-                interval=sub_parameter.interval,
-                rates=sub_parameter.rates,
-                slippages=sub_parameter.slippages,
-                sizes=sub_parameter.sizes,
-                priceticks=sub_parameter.priceticks,
-                capital=sub_parameter.capital,
-                inverse=sub_parameter.inverse,
-                annual_days=sub_parameter.annual_days,
-                mode=sub_parameter.mode,
+            ab_symbols=ab_symbols,
+            interval=sub_parameter.interval,
+            rates=sub_parameter.rates,
+            slippages=sub_parameter.slippages,
+            sizes=sub_parameter.sizes,
+            priceticks=sub_parameter.priceticks,
+            capital=sub_parameter.capital,
+            inverse=sub_parameter.inverse,
+            annual_days=sub_parameter.annual_days,
+            mode=sub_parameter.mode,
         )
-        strategy = strategy_class(replay_runner, strategy_name, ab_symbols, setting)
+        strategy = strategy_class(
+            replay_runner, strategy_name, ab_symbols, setting)
         self.strategies[strategy_name] = strategy
 
-        self.replay_runner[strategy_name] = replay_runner
+        self.replay_runners[strategy_name] = replay_runner
 
     def remove_strategy(self, strategy_name: str):
         self.strategies.pop(strategy_name)
-        self.replay_runner.pop(strategy_name)
+        self.replay_runners.pop(strategy_name)
 
     def get_strategy(self, strategy_name: str):
         strategy = self.strategies.get(strategy_name, None)
@@ -71,18 +72,24 @@ class BacktestStrategyRunner(StrategyManager):
         strategy = self.strategies[strategy_name]
         strategy.update_setting(setting)
 
-    # TODO ret
-    def run_backtest(self, start_dt: datetime, end_dt: datetime, interval: Interval = Interval.MINUTE, output_log: bool=False):
+    def run_backtest(
+        self, start_dt: datetime, end_dt: datetime,
+        interval: Interval = Interval.MINUTE, output_log: bool = False
+    ) -> Tuple[OrderedDict[Text, Iterable[TradeData]], OrderedDict[Text, Iterable[ContractsDailyResult]], OrderedDict[Text, Dict]]:
         assert interval == Interval.MINUTE, "for now, only Minute interval backtest are supported. Tick level may supported later."
         if self.data_loader is None:
             raise AttributeError("data_loader is not set")
+        trade_datas, daily_results, statistics = OrderedDictionary(), OrderedDictionary(), OrderedDictionary()
+
+        start_dt = start_dt.date()
+        end_dt = end_dt.date()
         while self.strategies:
             strategy_name, strategy = self.strategies.popitem()
             ab_symbols = strategy.ab_symbols
-            
-            replay_runner = self.replay_runner.pop(strategy_name)
+
+            replay_runner = self.replay_runners.pop(strategy_name)
             replay_runner.set_data_loader(self.data_loader)
-          
+
             # TODO
             # replay_runner.add_strategy()
             replay_runner.set_strategy(strategy)
@@ -93,5 +100,11 @@ class BacktestStrategyRunner(StrategyManager):
             print("simulate matching done========================")
             df = replay_runner.calculate_result()
             print("calculate done========================")
-            replay_runner.calculate_statistics(df)
+            statistic = replay_runner.calculate_statistics(df)
+
+            trade_datas[strategy_name] = replay_runner.sorted_trades()
+            daily_results[strategy_name] = replay_runner.sorted_daily_results()
+            statistics[strategy_name] = statistic
             print("calculate static done========================")
+        
+        return trade_datas, daily_results, statistics
