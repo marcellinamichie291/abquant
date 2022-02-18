9c3acaf7613eea8101cb8ad930d925be0a7177d2
zhanghui <zhanghui@abakusglobal.com>
2022-02-18 11:38:40 +0800

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import PackageFinder, setup, find_packages
 
 setup(
     name='abquant',
-    version='0.10.9',
+    version='0.10.10',
     description='event-driven quant dev tools',
     author='independent regime',
     # package_dir={"":"./abquant"},
