54a4931853101cb3cf5465242de58a6383cabab9
zhanghui <zhanghui@abakusglobal.com>
2022-05-20 14:58:31 +0800

    changelog

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import PackageFinder, setup, find_packages
 
 setup(
     name='abquant',
-    version='1.1.7',
+    version='1.1.8',
     description='event-driven quant dev tools',
     author='independent regime',
     # package_dir={"":"./abquant"},
