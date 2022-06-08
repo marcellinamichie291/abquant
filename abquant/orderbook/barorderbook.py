6ed6fe7311f2a9dd8214642d9a22bed477e6e4d2
weijianan <weijianan@abakusglobal.com>
2022-06-08 17:38:47 +0800

    stop market add

--- abquant/orderbook/barorderbook.py
+++ abquant/orderbook/barorderbook.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from itertools import chain
 from typing import Dict, Iterable, Optional, Tuple, overload
 
 from abquant.trader.common import Direction, Interval, OrderType, Status
@@ -48,9 +49,9 @@ class BarOrderBook(OrderBook):
                 "BarOrderBook.match_orders: bars not all in the same minute, bars, {}".format(self.bars))
 
         for order in list(self.active_limit_orders.values()):
-            if order.type != OrderType.LIMIT:
+            if order.type not in self.SUPPORTED_ORDERTYPE:
                 raise ValueError(
-                    "ordertype other than limit order are not supported yet.")
+                    "ordertype other than {} are not supported yet.".format(self.SUPPORTED_ORDERTYPE))
             bar = self.bars[order.ab_symbol]
 
             long_cross_price = bar.low_price
@@ -83,6 +84,60 @@ class BarOrderBook(OrderBook):
                 trade_price = min(order.price, long_best_price)
             else:
                 trade_price = max(order.price, short_best_price)
+
+            self.trade_count += 1
+            trade = TradeData(
+                symbol=order.symbol,
+                exchange=order.exchange,
+                orderid=order.orderid,
+                tradeid=str(self.trade_count),
+                direction=order.direction,
+                offset=order.offset,
+                price=trade_price,
+                volume=order.volume,
+                datetime=bar.datetime,
+                gateway_name=order.gateway_name,
+            )
+            yield (order, trade)
+        
+
+        for order in list(self.stop_market_orders.values()):
+            if order.type not in self.SUPPORTED_ORDERTYPE:
+                raise ValueError(
+                    "ordertype other than {} are not supported yet.".format(self.SUPPORTED_ORDERTYPE))
+            bar = self.bars[order.ab_symbol]
+
+            long_cross_price = bar.high_price
+            short_cross_price = bar.low_price
+            long_worst_price = bar.open_price
+            short_worst_price = bar.open_price
+
+            long_cross = (
+                order.direction == Direction.LONG
+                and order.price <= long_cross_price
+                and long_cross_price > 0
+            )
+
+            short_cross = (
+                order.direction == Direction.SHORT
+                and order.price >= short_cross_price
+                and short_cross_price > 0
+            )
+
+            if not long_cross and not short_cross:
+                continue
+
+            # Push order update with status "all traded" (filled).
+            order.traded = order.volume
+            order.status = Status.ALLTRADED
+
+            if long_cross:
+                trade_price = long_worst_price if order.price < long_worst_price else order.price
+            else:
+                trade_price = short_worst_price if order.price > short_worst_price else order.price
+
+            self.stop_market_orders.pop(order.ab_orderid)
+
 
             self.trade_count += 1
             trade = TradeData(
--- abquant/orderbook/orderbook.py
+++ abquant/orderbook/orderbook.py
@@ -1,11 +1,14 @@
 from abc import ABC, abstractmethod
+from itertools import chain
 from typing import Any, Dict, Iterable, Optional, Tuple
 from abquant.trader.common import OrderType, Status
 from abquant.trader.msg import DepthData, BarData, EntrustData, OrderData, TickData, TradeData
 
 class OrderBook(ABC):
+    SUPPORTED_ORDERTYPE = {OrderType.LIMIT, OrderType.STOP_MARKET}
     def __init__(self):
         self.active_limit_orders: Dict[str, OrderData] = {}
+        self.stop_market_orders: Dict[str, OrderData] = {}
 
     @staticmethod
     def orderbook_factory(mode: str) -> "OrderBook":
@@ -22,17 +25,23 @@ class OrderBook(ABC):
     def insert_order(self, order: OrderData) -> str:
         self.pre_matching()
         assert(order.status == Status.SUBMITTING)
-        if order.type != OrderType.LIMIT:
-            raise ValueError("order type other than limit are not going to support.")
-        self.active_limit_orders[order.ab_orderid] = order
+        if order.type not in self.SUPPORTED_ORDERTYPE:
+            raise ValueError("{} order rerceived. order type other than {} are not going to support.".format(order.type, self.SUPPORTED_ORDERTYPE))
+        if order.type == OrderType.LIMIT:
+            self.active_limit_orders[order.ab_orderid] = order
+        elif order.type == OrderType.STOP_MARKET:
+            self.stop_market_orders[order.ab_orderid] = order
         return order.ab_orderid
 
     @abstractmethod
     def cancel_order(self, ab_orderid: str) -> Optional[OrderData]:
         self.pre_matching()
-        if ab_orderid not in self.active_limit_orders:
+        if ab_orderid in self.active_limit_orders:
+            order = self.active_limit_orders.pop(ab_orderid)
+        elif ab_orderid in self.stop_market_orders:
+            order = self.stop_market_orders.pop(ab_orderid)
+        else:
             return None
-        order = self.active_limit_orders.pop(ab_orderid)
         order.status = Status.CANCELLED
         return order
     
@@ -43,7 +52,7 @@ class OrderBook(ABC):
 
     def submitting_orders(self) -> Iterable[OrderData]:
         self.pre_matching()
-        return (order for order in self.active_limit_orders.values() if order.status == Status.SUBMITTING)
+        return (order for order in chain(self.active_limit_orders.values(), self.stop_market_orders.values()) if order.status == Status.SUBMITTING)
     
     def accept_submitting_orders(self) -> Iterable[OrderData]:
         self.pre_matching()
